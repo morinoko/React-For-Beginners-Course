6592e84d4b0ebac1ed4946366434e45bfdc0b71d
Felice Forby <forby.1@gmail.com>
2020-10-10 13:29:42 +0900

    Use props and stateless functional component for Header

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,23 +1,44 @@
 import React from 'react';
 
-class Header extends React.Component {
-	render() {
-		return (
-			<header className="top">
-				<h1>
-					Catch
-					<span className="ofThe">
-						<span className="of">Of</span>
-						<span className="the">The</span>
-					</span>
-					Day
-				</h1>
-				<h3 className="tagline">
-					<span>Fresh Seafood Market</span>
-				</h3>
-			</header>
-		)
-	}
-}
+// Stateless Functional Component (Only needs to render some)
+const Header = ({tagline}) => (
+	<header className="top">
+		<h1>
+			Catch
+			<span className="ofThe">
+				<span className="of">Of</span>
+				<span className="the">The</span>
+			</span>
+			Day
+		</h1>
+		<h3 className="tagline">
+			<span>{tagline}</span>
+		</h3>
+	</header>
+);
 
 export default Header;
+
+// Regular React Component
+
+// class Header extends React.Component {
+// 	render() {
+// 		return (
+// 			<header className="top">
+// 				<h1>
+// 					Catch
+// 					<span className="ofThe">
+// 						<span className="of">Of</span>
+// 						<span className="the">The</span>
+// 					</span>
+// 					Day
+// 				</h1>
+// 				<h3 className="tagline">
+// 					<span>{this.props.tagline}</span>
+// 				</h3>
+// 			</header>
+// 		)
+// 	}
+// }
+
+// 'this' refers to the component instance
