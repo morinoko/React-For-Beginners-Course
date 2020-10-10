fb3d5cab0f221cf1fce6edb54e94df6baa7c1b5d
Felice Forby <forby.1@gmail.com>
2020-10-10 15:15:47 +0900

    Set up form to add fish and update state

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,25 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+	// Methods that set state and methods that update state
+	// always need to live in the exact same component
+	state = {
+		fishes: {},
+		order: {}
+	};
+
+	addFish = (fish) => {
+		// 1. Take a copy of exisiting state (because we never want to directly mutate state)
+		const fishes = {...this.state.fishes};
+		// 2. Add a new fish to the copied variable
+		fishes[`fish${Date.now()}`] = fish;
+		// 3. Set the new fishes object to state
+		//    Only updates the pieces you give it
+		this.setState({
+			fishes // same as fishes: fishes
+		});
+	}
+
 	render() {
 		return (
 			<div className="catch-of-the-day">
@@ -11,7 +30,7 @@ class App extends React.Component {
 					<Header tagline="Fresh Seafood Market" />
 				</div>
 				<Order />
-				<Inventory />
+				<Inventory addFish={this.addFish} />
 			</div>
 		)
 	}
